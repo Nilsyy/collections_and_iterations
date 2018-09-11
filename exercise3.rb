f88badeaf182939713ddbc3c7414588da0c0daed
Nils Yuan <Nilsyuan@gmail.com>
2018-09-11 16:24:31 -0400

    I finished number 3.

---
+++ exercise3.rb
@@ -0,0 +1,43 @@
+fav_colours = ["green", "yellow", "pink", "black", "silver"]
+age = [17, 17, 27, 21, 18]
+coin_flip = ["heads", "tails", "heads", "heads", "tails"]
+artists = [:Enya, :Grimes, :RZA]
+fav_colourss = [:green, :yellow, :pink, :black, :silver]
+
+three_words =
+{ :love => "Just a chemical reaction that compels animals to breed",
+  :hemp => "The male Cannabis plant that has a THC too low to get high on.",
+  :pig => "A very cute animal."
+}
+
+fav_movies =
+{ :LOTR_TROTK => "2001",
+  :Inuyashiki => "2018",
+  :The_Lovely_Bones => "2009",
+  :Mrs_Doubtfire => "1993"
+}
+
+city =
+{
+  :Tokyo => "38,305,000",
+  :Toronto => "6,418,000",
+  :New_York => "20,300,000",
+}
+
+names =
+{
+  :Andy => 17,
+  :Sena => 17,
+  :Lucas => 9,
+  :Stella => 6,
+  :Amy => 23,
+  :Miroslav => 22
+}
+
+puts "#{artists[0]}"
+puts "#{artists[1]}"
+fav_movies.each { |fav_movies, year| puts "#{fav_movies} came out in #{year}" }
+reverse_names = names.sort_by { |name, age| age }.reverse
+puts reverse_names
+fav_movies["Beauty and the Beast"] = [1991, 2017]
+puts fav_movies["Beauty and the Beast"]
